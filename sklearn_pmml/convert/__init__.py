6f12c752e534a5f30e992f7f893e4e3570ba2afc
YuHuaCheng <yc2911@columbia.edu>
2015-10-07 17:09:24 -0400

    import random_forest in __init__ to update estimator_to_converter in utils

--- sklearn_pmml/convert/__init__.py
+++ sklearn_pmml/convert/__init__.py
@@ -2,6 +2,7 @@ from sklearn_pmml import pmml
 from sklearn_pmml.convert.features import Feature, NumericFeature, CategoricalFeature, RealNumericFeature
 from sklearn_pmml.convert.gbrt import *
 from sklearn_pmml.convert.tree import *
+from sklearn_pmml.convert.random_forest import *
 from sklearn_pmml.convert.model import *
 from sklearn_pmml.convert.utils import *
 
